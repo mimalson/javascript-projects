36f47dc03515232aae32ff4083514db5a703dfb9
Amina <87755052+mimalson@users.noreply.github.com>
2022-04-20 21:15:02 +0100

    Update index.js

--- ThemeSwitcher/index.js
+++ ThemeSwitcher/index.js
@@ -4,6 +4,6 @@ input.addEventListener("change", (e) => {
   if (e.target.checked) {
     document.body.setAttribute("data-theme", 'dark');
   } else {
-    document.body.setAttribute('data-theme', 'light');
+    document.body.setAttribute("data-theme", 'light');
   }
 })
