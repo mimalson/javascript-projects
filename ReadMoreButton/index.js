acc620fda8ba57c54a4c3c6dfb2ad61573fa877c
Amina <87755052+mimalson@users.noreply.github.com>
2022-04-25 22:28:40 +0100

    Update index.js

--- ReadMoreButton/index.js
+++ ReadMoreButton/index.js
@@ -1,2 +1,5 @@
 const readMoreBtn = document.querySelector('.readMoreBtn');
 const text = document.querySelector('.text');
+readMoreBtn.addEventListener('click',(e)=>{
+  text.classList.toggle('show-more');
+  if(readMoreBtn.innerText === 'Read More'){
